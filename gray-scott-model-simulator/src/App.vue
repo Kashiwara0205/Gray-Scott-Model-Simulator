b876158b476826f8e563fadd01f52e645f23e7fa
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2020-12-28 21:05:50 +0900

    Modify user interface
    - moved operation panel to right position
    - change SQUARE_SIZE 20 to 10

--- gray-scott-model-simulator/src/App.vue
+++ gray-scott-model-simulator/src/App.vue
@@ -22,8 +22,8 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
+
 </style>
--- gray-scott-model-simulator/src/components/GrayScotModelSimulator.vue
+++ gray-scott-model-simulator/src/components/GrayScotModelSimulator.vue
@@ -1,27 +1,31 @@
 <template>
-  <div>
-    <canvas id="gray_scot_model_canvas" width="500" height="500" class="canvas"></canvas>
-
+  <div id="gray-scot-model-simulator-container">
+    <div id="output">
+      <canvas id="gray_scot_model_canvas" width="500" height="500" class="canvas"></canvas>
+    </div>
     <br>
+    <div id="operation" style="padding-top: 5px;">
 
-    <div>
-
-      <button type="button">
-        <font size="5" color="#333399" @click="onStart(feed, kill)">start</font>
-      </button>
-
-      <button type="button" style="margin-left: 20px;">
-         <font size="5" color="#333399" @click="onStop">stop</font>
-      </button>
-
-      <font style="margin-left: 20px;" size="5" color="#333399">feed: </font> 
+      <font style="margin-left: 20px; padding-top: 50px;" size="5" color="#333399">feed: </font> 
       <input type="number" size="10" v-model.number="feed" maxlength="10"> 
-
       <font style="margin-left: 20px;" size="5" color="#333399">kill: </font> 
       <input type="number" size="10" v-model.number="kill" maxlength="10"> 
 
-    </div> 
+      <br>
 
+      <button style="margin-left: 20px; margin-top: 10px;" type="button">
+        <font size="5" color="#333399" @click="onStart(feed, kill)">Start</font>
+      </button>
+
+      <button style="margin-left: 20px;" type="button">
+         <font size="5" color="#333399" @click="onStop">Stop</font>
+      </button>
+
+      <button style="margin-left: 20px;" type="button">
+         <font size="5" color="#333399" @click="onInit">Init</font>
+      </button>
+
+    </div> 
   </div>
 </template>
 
@@ -30,8 +34,10 @@ import { Component, Vue } from 'vue-property-decorator';
 import { GrayScotModelFactory } from "../lib/gray-scott-model/gray-scott-model"
 
 const SPACE_GRIDSIZE = 100
-const SQUARE_SIZE = 20
-const VISUALIZATION_STEP = 10
+const SQUARE_SIZE = 10
+const VISUALIZATION_STEP = 15
+const CANVAS_WIDTH = 500
+const CANVAS_HEIGHT = 500
 
 @Component
 export default class GrayScotModelSimulator extends Vue {
@@ -43,11 +49,12 @@ export default class GrayScotModelSimulator extends Vue {
 
   private mounted(){  
     this.canvas = document.getElementById('gray_scot_model_canvas')
+    this.onInit()
   }
 
   private onStart(feed: number, kill: number){
     clearInterval(this.interval);
-    this.grayScotModel = (new GrayScotModelFactory).create(feed, kill, SPACE_GRIDSIZE, SQUARE_SIZE)
+    this.grayScotModel = this.createGrayScotModel(feed, kill)
     this.onDraw(this.grayScotModel.materialU)
     this.interval = setInterval(this.onUpdate, 0.001)
   }
@@ -61,10 +68,18 @@ export default class GrayScotModelSimulator extends Vue {
     this.onDraw(this.grayScotModel.materialU)
   }
 
+  private onInit(){
+    clearInterval(this.interval);
+    this.$set(this, "feed", 0.022)
+    this.$set(this, "kill", 0.051)
+    this.grayScotModel = this.createGrayScotModel(0.022, 0.051)
+    this.onDraw(this.grayScotModel.materialU)
+  }
+
   private onDraw(materialU){
     const ctx = this.canvas.getContext('2d')
-    const cellWidth = Math.floor(this.canvas.width / SPACE_GRIDSIZE)
-    const cellHeight = Math.floor(this.canvas.height / SPACE_GRIDSIZE)
+    const cellWidth = Math.floor(CANVAS_WIDTH / SPACE_GRIDSIZE)
+    const cellHeight = Math.floor(CANVAS_HEIGHT / SPACE_GRIDSIZE)
 
     for(let i = 0; i < SPACE_GRIDSIZE; i++){
       for(let j = 0; j < SPACE_GRIDSIZE; j++){
@@ -86,6 +101,10 @@ export default class GrayScotModelSimulator extends Vue {
       }
     }
   }
+
+  private createGrayScotModel(feed: number, kill: number){
+    return (new GrayScotModelFactory).create(feed, kill, SPACE_GRIDSIZE, SQUARE_SIZE)
+  }
 }
 </script>
 
@@ -93,5 +112,23 @@ export default class GrayScotModelSimulator extends Vue {
 <style scoped>
 .canvas {
   border: 1px solid #000;
+}
+
+#gray-scot-model-simulator-container {
+  display: grid;
+  grid-template-rows: 500px 1fr;
+  grid-template-columns: 500px 1fr;
+}
+
+#output{
+  grid-row: 1 / 3;
+  grid-column: 1 / 2;
+}
+
+#operation{
+  grid-row: 1 / 3;
+  grid-column: 2 / 4;
+  background: #8f8;
+  padding-left: 10px;
 }
 </style>
